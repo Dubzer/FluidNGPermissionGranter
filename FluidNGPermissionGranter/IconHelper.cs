299a0e9f0c078a0dee2c2fdb8a86be8e74ef5d8c
Dubzer <ship8577@gmail.com>
2018-12-04 12:14:42 +0300

    some refactoring

--- FluidNGPermissionGranter/IconHelper.cs
+++ FluidNGPermissionGranter/IconHelper.cs
@@ -5,41 +5,32 @@ using System.Runtime.InteropServices;
 using System.Windows;
 using System.Windows.Interop;
 
-public static class IconHelper
+namespace FluidNGPermissionGranter
 {
-    [DllImport("user32.dll")]
-    static extern int GetWindowLong(IntPtr hwnd, int index);
-
-    [DllImport("user32.dll")]
-    static extern int SetWindowLong(IntPtr hwnd, int index, int newStyle);
-
-    [DllImport("user32.dll")]
-    static extern bool SetWindowPos(IntPtr hwnd, IntPtr hwndInsertAfter,
-        int x, int y, int width, int height, uint flags);
-
-    [DllImport("user32.dll")]
-    static extern IntPtr SendMessage(IntPtr hwnd, uint msg,
-        IntPtr wParam, IntPtr lParam);
-
-    const int GWL_EXSTYLE = -20;
-    const int WS_EX_DLGMODALFRAME = 0x0001;
-    const int SWP_NOSIZE = 0x0001;
-    const int SWP_NOMOVE = 0x0002;
-    const int SWP_NOZORDER = 0x0004;
-    const int SWP_FRAMECHANGED = 0x0020;
-    const uint WM_SETICON = 0x0080;
-
-    public static void RemoveIcon(Window window)
+    public static class IconHelper
     {
-        // Get this window's handle
-        IntPtr hwnd = new WindowInteropHelper(window).Handle;
+        [DllImport("user32.dll")]
+        static extern int GetWindowLong(IntPtr hwnd, int index);
 
-        // Change the extended window style to not show a window icon
-        int extendedStyle = GetWindowLong(hwnd, GWL_EXSTYLE);
-        SetWindowLong(hwnd, GWL_EXSTYLE, extendedStyle | WS_EX_DLGMODALFRAME);
+        [DllImport("user32.dll")]
+        static extern int SetWindowLong(IntPtr hwnd, int index, int newStyle);
 
-        // Update the window's non-client area to reflect the changes
-        SetWindowPos(hwnd, IntPtr.Zero, 0, 0, 0, 0, SWP_NOMOVE |
-              SWP_NOSIZE | SWP_NOZORDER | SWP_FRAMECHANGED);
+        [DllImport("user32.dll")]
+        static extern bool SetWindowPos(IntPtr hwnd, IntPtr hwndInsertAfter, int x, int y, int width, int height, uint flags);
+
+        const int GWL_EXSTYLE = -20;
+        const int WS_EX_DLGMODALFRAME = 0x0001;
+        const int SWP_NOSIZE = 0x0001;
+        const int SWP_NOMOVE = 0x0002;
+        const int SWP_NOZORDER = 0x0004;
+        const int SWP_FRAMECHANGED = 0x0020;
+
+        public static void RemoveIcon(Window window)
+        {
+            IntPtr hwnd = new WindowInteropHelper(window).Handle;
+            int extendedStyle = GetWindowLong(hwnd, GWL_EXSTYLE);
+            SetWindowLong(hwnd, GWL_EXSTYLE, extendedStyle | WS_EX_DLGMODALFRAME);
+            SetWindowPos(hwnd, IntPtr.Zero, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE | SWP_NOZORDER | SWP_FRAMECHANGED);
+        }
     }
 }
