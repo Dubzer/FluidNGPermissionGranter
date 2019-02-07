246ea0dadd6afc878be3f73421ebda0b3463113e
Dubzer <dubzer@ya.ru>
2019-02-07 23:10:01 +0300

    probability of exception is very small now
    ...and code refactoring

--- FluidNGPermissionGranter/MainWindow.xaml.cs
+++ FluidNGPermissionGranter/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Diagnostics;
 using System.Linq;
 using System.Net;
+using System.Threading;
 using System.Windows;
 using System.Windows.Threading;
 using SharpAdbClient;
@@ -17,37 +18,18 @@ namespace FluidNGPermissionGranter
 
         private ADBGuide adbGuideWindow;    //  There you can find guide about activating ADB on phone
         private ConnectGuide connectWindow;
-        private AllowADBWindow allowADBWindow;
+        private AllowADBWindow allowAdbWindow;
         private DoneWindow doneWindow;
+        private readonly Action phoneAuthorized;
+
+        private static DeviceData device;
+
 
         public MainWindow() 
         {
             InitializeComponent();
             adbPath = (System.IO.Directory.GetCurrentDirectory() + @"\adb\adb.exe"); // Getting path of ADB tools 
-        }
-
-        // Stopping ADB server after closing program
-        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
-        {
-            StopApplication();
-        }
-
-        public static void StopApplication()
-        {
-            //  Trying to kill ADB Server process 
-            try
-            {
-                foreach (var proc in Process.GetProcessesByName("adb"))
-                {
-                    proc.Kill();
-                }
-            }
-            //  If can't => just closing app
-            catch
-            {
-                Environment.Exit(0);
-            }
-            Environment.Exit(0);
+            phoneAuthorized += AfterAuthorization;  // Setiing method for action 
         }
 
         // Removing icon on the top of the window 
@@ -64,6 +46,7 @@ namespace FluidNGPermissionGranter
             adbGuideWindow = new ADBGuide { ShowInTaskbar = false, Owner = Application.Current.MainWindow };
             adbGuideWindow.ShowDialog();
         }
+
         //  The second button
         private void GrantButton_Click(object sender, RoutedEventArgs e)
         {
@@ -77,7 +60,7 @@ namespace FluidNGPermissionGranter
                 {
                     StartDeviceMonitor();
                 }
-                //  If device is not connected => Show help window 
+                //  If device is not connected => Show help window
                 else
                 {
                     connectWindow = new ConnectGuide{ Title = "", Owner = Application.Current.MainWindow };
@@ -95,6 +78,7 @@ namespace FluidNGPermissionGranter
                 }
             }
         }
+
         #endregion
 
         private void StartAdb()
@@ -123,50 +107,18 @@ namespace FluidNGPermissionGranter
 
                 monitor.Start();
             }
-        }
-
-        private static string SendToAdb(string command)
-        {
-            //  Trying to send command to device via ADB
-            try
-            {
-                var device = AdbClient.Instance.GetDevices().First();
-                var receiver = new ConsoleOutputReceiver();
-                AdbClient.Instance.ExecuteRemoteCommand(command, device, receiver);
-                return receiver.ToString();
-            }
-            // if can't => show error message and give a link to contact developer
-            catch(Exception exception)
-            {
-                MessageBoxResult result = MessageBox.Show("Can`t send command by ADB. Please check your connection and try again. Also, try to redo the second step. Moreover, you can ask for it to the developer: \n \n" + exception, "Error", MessageBoxButton.YesNo);
-                if (result == MessageBoxResult.Yes)
-                {
-                    Process.Start("https://dubzer.github.io");
-                }
-                return exception.ToString();
-            }
-        }
-
-        private void GrantPermission()
-        {
-            string output = SendToAdb("pm grant com.fb.fluid android.permission.WRITE_SECURE_SETTINGS");    //  Trying to grant permission 
-            //  If output is *nothing*, then it means that there is no any errors, and we can show Successful window
-            if (output == "")
-            {
-                //  Showing "done" window 
-                doneWindow = new DoneWindow() { Title = "", Owner = Application.Current.MainWindow };
-                doneWindow.ShowDialog();
-                //  Restarting Fluid Navigation Gestures app on device
-                SendToAdb("am force-stop com.fb.fluid");
-                SendToAdb("am start -n com.fb.fluid/com.fb.fluid.ActivityMain");
-            }
             else
             {
-                MessageBox.Show("Unexpected output: \n \n" + output);
+                monitor = null;
+                monitor = new DeviceMonitor(new AdbSocket(new IPEndPoint(IPAddress.Loopback, AdbClient.AdbServerPort)));
+                monitor.DeviceConnected += OnDeviceConnected;
+
+                monitor.Start();
+
             }
         }
         
-        //  What happens after ` connected
+        //  What happens after device connected
         private void OnDeviceConnected(object sender, DeviceDataEventArgs e)
         {
             Dispatcher.BeginInvoke(DispatcherPriority.Normal, new Action(() =>
@@ -193,36 +145,67 @@ namespace FluidNGPermissionGranter
                         }
                         break;
                     case DeviceState.Unauthorized:
-                        //Showing help window to authorize PC
-                        allowADBWindow = new AllowADBWindow();
+                        device = AdbClient.Instance.GetDevices().First();
+                        Debug.Print(device.GetType().ToString());
 
-                        allowADBWindow.Show();
-                        allowADBWindow.ContentRendered += AllowADBWindow_ContentRendered;
+
+                        //Showing help window to authorize PC
+                        allowAdbWindow = new AllowADBWindow();
+                        
+                        allowAdbWindow.Show();
+                        AdbClient.Instance.GetDevices();
+
+                        allowAdbWindow.ContentRendered += AllowADBWindow_ContentRendered;
                         break;
                 }
             }));
         }
 
-        //  Shit, but without it not works =(
         private void AllowADBWindow_ContentRendered(object sender, EventArgs e)
         {
-            while (AdbClient.Instance.GetDevices().Any() && AdbClient.Instance.GetDevices().First().State == DeviceState.Unauthorized) { } //  Waiting to get Online phone 
+            Thread authorizationCheckThread = new Thread(StartCheckForAuthorization);
+            authorizationCheckThread.Start();
+        }
 
-            if (!AdbClient.Instance.GetDevices().Any() || AdbClient.Instance.GetDevices().First().State == DeviceState.Offline || AdbClient.Instance.GetDevices().First().State == DeviceState.Unknown)
+        private void StartCheckForAuthorization()
+        {
+            //  Waiting to get Online phone
+            while (AdbClient.Instance.GetDevices().Any() &&
+                   AdbClient.Instance.GetDevices().First().State == DeviceState.Unauthorized)
             {
-                allowADBWindow.Close();
-                allowADBWindow = null;
-                connectWindow = null;
-                connectWindow = new ConnectGuide { Title = "", Owner = Application.Current.MainWindow };
-                connectWindow.ShowDialog();
+                Thread.Sleep(300);
+            }
+
+            phoneAuthorized();
+        }
+
+        private void AfterAuthorization()
+        {
+            if (!AdbClient.Instance.GetDevices().Any() ||
+                AdbClient.Instance.GetDevices().First().State == DeviceState.Offline ||
+                AdbClient.Instance.GetDevices().First().State == DeviceState.Unknown)
+            {
+                Dispatcher.BeginInvoke(DispatcherPriority.Normal, new Action(() =>
+                {
+                    allowAdbWindow.Close();
+                    allowAdbWindow = null;
+                    connectWindow = null;
+                    connectWindow = new ConnectGuide {Title = "", Owner = Application.Current.MainWindow};
+                    connectWindow.ShowDialog();
+                }));
             }
             else
             {
-                allowADBWindow.Close();
-                GrantButton.IsEnabled = true;
+                Dispatcher.BeginInvoke(DispatcherPriority.Normal, new Action(() =>
+                {
+                    allowAdbWindow.Close();
+                    GrantButton.IsEnabled = true;
+                }));
+
                 try
                 {
                     GrantPermission();
+
                 }
                 catch (Exception exception)
                 {
@@ -230,8 +213,104 @@ namespace FluidNGPermissionGranter
                     throw;
                 }
 
-                allowADBWindow.ContentRendered -= AllowADBWindow_ContentRendered;
+                allowAdbWindow.ContentRendered -= AllowADBWindow_ContentRendered;
             }
+        }
+
+        private static string SendToAdb(string command)
+        {
+            //  Trying to send command to device via ADB
+            try
+            {
+                device = AdbClient.Instance.GetDevices().First();
+                var receiver = new ConsoleOutputReceiver();
+                AdbClient.Instance.ExecuteRemoteCommand(command, device, receiver);
+                return receiver.ToString();
+            }
+            // if can't => show error message and give a link to contact developer
+            catch (Exception exception)
+            {
+                MessageBoxResult result = MessageBox.Show("Can`t send command by ADB. Please check your connection and try again. Also, try to redo the second step. Moreover, you can ask for it to the developer: \n \n" + exception, "Error", MessageBoxButton.YesNo);
+                if (result == MessageBoxResult.Yes)
+                {
+                    Process.Start("https://dubzer.github.io");
+                }
+                return exception.ToString();
+            }
+        }
+
+        private void GrantPermission()
+        {
+            string output = SendToAdb("pm grant com.fb.fluid android.permission.WRITE_SECURE_SETTINGS");    //  Trying to grant permission 
+            //  If output is *nothing*, then it means that there is no any errors, and we can show Successful window
+            if (output == "")
+            {
+                //  Showing "done" window 
+                Dispatcher.BeginInvoke(DispatcherPriority.Normal, new Action(() =>
+                {
+                    doneWindow = new DoneWindow() { Title = "", Owner = Application.Current.MainWindow };
+                    doneWindow.ShowDialog();
+
+                }));
+
+                //  Restarting Fluid Navigation Gestures app on device
+                SendToAdb("am force-stop com.fb.fluid");
+                SendToAdb("am start -n com.fb.fluid/com.fb.fluid.ActivityMain");
+            }
+            else
+            {
+                MessageBox.Show("Unexpected output: \n \n" + output);
+            }
+        }
+
+        // Stopping ADB server after closing program
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            #region closing all windows
+
+            if (adbGuideWindow != null)
+            {
+                adbGuideWindow.Close();
+                adbGuideWindow = null;
+            }
+            else if (connectWindow != null)
+            {
+                connectWindow.Close();
+                connectWindow = null;
+            }
+            else if (allowAdbWindow != null)
+            {
+                allowAdbWindow.Close();
+                allowAdbWindow = null;
+            }
+            else if (doneWindow != null)
+            {
+                doneWindow.Close();
+                doneWindow = null;
+            }
+
+            #endregion
+
+            StopApplication();
+        }
+
+        public static void StopApplication()
+        {
+
+            //  Trying to kill ADB Server process 
+            try
+            {
+                foreach (var proc in Process.GetProcessesByName("adb"))
+                {
+                    proc.Kill();
+                }
+            }
+            //  If can't => just closing app
+            catch
+            {
+                Environment.Exit(0);
+            }
+            Environment.Exit(0);
         }
     }
 }
